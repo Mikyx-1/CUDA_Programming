c4bbbbec16a44052d923e42de3d06e90415f742e
le_hoang_viet <le_hoang_viet@monorevo.jp>
2025-06-28 09:52:17 +0700

    revert the flash_attention to the original code, change the main.cpp and test.py in attn/ for flash_attention

--- attn/main.cpp
+++ attn/main.cpp
@@ -1,8 +1,8 @@
 #include <torch/extension.h>
 
-torch::Tensor self_attention(torch::Tensor Q, torch::Tensor K, torch::Tensor V);
+torch::Tensor forward(torch::Tensor Q, torch::Tensor K, torch::Tensor V);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
-    m.def("self_attention", torch::wrap_pybind_function(self_attention), "Calculate self-attention of Q, K, V");
+    m.def("forward", torch::wrap_pybind_function(forward), "Calculate self-attention of Q, K, V using flash attention algorithm");
 }
--- attn/test.py
+++ attn/test.py
@@ -4,16 +4,16 @@ from torch.utils.cpp_extension import load
 
 ext = load(
     name="softmax",
-    sources=["attn/main.cpp", "attn/self_attention.cu"],
+    sources=["attn/main.cpp", "attn/flash_attention.cu"],
     extra_cuda_cflags=["-O2"],
     extra_cflags=["-O2"],
 )
 
 
-DEVICE = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-q = torch.randn((2, 4, 16)).to(DEVICE)
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+q = torch.randn((2, 2, 4, 16)).to(DEVICE)
 k = torch.randn_like(q)
 v = torch.randn_like(q)
 
-res = ext.self_attention(q, k, v)
+res = ext.forward(q, k, v)
 print(f"res: {res}")
