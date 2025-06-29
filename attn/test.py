7f63043f61b68c5d153aafa7e5fee8b3269fe71c
le_hoang_viet <le_hoang_viet@monorevo.jp>
2025-06-29 11:56:20 +0700

    add time_benchmarking to test.py in attn/

--- attn/test.py
+++ attn/test.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn.functional as F
 from torch.utils.cpp_extension import load
+import math
+import time
 
 ext = load(
     name="softmax",
@@ -11,9 +13,28 @@ ext = load(
 
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-q = torch.randn((2, 2, 32, 16)).to(DEVICE)  # Currently, must be divisible by 32
+q = torch.randn((2, 16, 48, 13)).to(DEVICE)  # Currently, must be divisible by 32
 k = torch.randn_like(q)
 v = torch.randn_like(q)
 
-res = ext.flash_attention(q, k, v)
-print(f"res: {res}")
+def manual_attn(q, k, v):
+    att = (q @ k.transpose(-2, -1) * (1.0 / math.sqrt(k.size(-1))))
+    att = F.softmax(att, dim=-1)
+    y = att @ v
+    return y
+
+
+tic = time.time()
+for _ in range(1000):
+    res = ext.flash_attention(q, k, v)
+toc = time.time()
+print(f"Duration: {toc - tic}")
+
+tic = time.time()
+for _ in range(1000):
+    gt = manual_attn(q, k, v)
+toc = time.time()
+print(f"Duration: {toc - tic}")
+
+
+print(f"all_close: {torch.allclose(res, gt)}, Max Diff: {torch.abs(res - gt).max()}")
