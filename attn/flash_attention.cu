449a87091c68c7c4b907cc477a426aaebcf58380
le_hoang_viet <le_hoang_viet@monorevo.jp>
2025-06-29 11:55:20 +0700

    add guard to flash_attention.cu to make sure it can work on various sequence lengths

--- attn/flash_attention.cu
+++ attn/flash_attention.cu
@@ -1,18 +1,15 @@
 #include <torch/types.h>
 #include <cuda.h>
-#include <cuda.h>
-
-
+#include <cuda_runtime.h>  // you had cuda.h twice, cleaned that up
 
 __global__
 void forward_kernel(const float* Q, const float* K, const float* V, const int N, const int d, 
                     const int Tc, const int Tr, const int Bc, const int Br, const float softmax_scale,
                     float* l, float* m, float* O)
 {
-
-    int thread_id = threadIdx.x;    // Seq_id
-    int batch_id = blockIdx.x;      // Batch_id
-    int head_id = blockIdx.y;       // Head_id
+    int thread_id = threadIdx.x;
+    int batch_id = blockIdx.x;
+    int head_id = blockIdx.y;
 
     int qkv_offset = (batch_id * gridDim.y * N * d) + (head_id * N * d);
     int lm_offset = (batch_id * gridDim.y * N) + (head_id * N);
@@ -24,82 +21,95 @@ void forward_kernel(const float* Q, const float* K, const float* V, const int N,
     float* Vj = &sram[tile_size * 2];
     float* S = &sram[tile_size * 3];
 
-
     for (int tile_column_id = 0; tile_column_id < Tc; tile_column_id++)
     {
-        // Load Kj, Vj to SRAM
-        for (int dim_id = 0; dim_id < d; dim_id++)
-        {
-            Kj[(thread_id * d) + dim_id] = K[qkv_offset + (tile_column_id * tile_size) + (thread_id * d) + dim_id];
-            Vj[(thread_id * d) + dim_id] = V[qkv_offset + (tile_column_id * tile_size) + (thread_id * d) + dim_id];
+        int col_start = tile_column_id * Bc;
+        int global_col_id = col_start + thread_id;
+
+        // Load Kj, Vj with guard
+        if (global_col_id < N) {
+            for (int dim_id = 0; dim_id < d; dim_id++) {
+                Kj[thread_id * d + dim_id] = K[qkv_offset + global_col_id * d + dim_id];
+                Vj[thread_id * d + dim_id] = V[qkv_offset + global_col_id * d + dim_id];
+            }
+        } else {
+            for (int dim_id = 0; dim_id < d; dim_id++) {
+                Kj[thread_id * d + dim_id] = 0.0f;
+                Vj[thread_id * d + dim_id] = 0.0f;
+            }
         }
         __syncthreads();
 
         for (int tile_row_id = 0; tile_row_id < Tr; tile_row_id++)
         {
-            // Load Qi to SRAM
-            for (int dim_id = 0; dim_id < d; dim_id++)
-            {
-                Qi[(thread_id * d) + dim_id] = Q[qkv_offset + (tile_row_id * tile_size) + (thread_id * d) + dim_id];
+            int row_start = tile_row_id * Br;
+            int global_row_id = row_start + thread_id;
+
+            // Load Qi with guard
+            if (global_row_id < N) {
+                for (int dim_id = 0; dim_id < d; dim_id++) {
+                    Qi[thread_id * d + dim_id] = Q[qkv_offset + global_row_id * d + dim_id];
+                }
+            } else {
+                for (int dim_id = 0; dim_id < d; dim_id++) {
+                    Qi[thread_id * d + dim_id] = 0.0f;
+                }
             }
 
+            float row_m_prev = (global_row_id < N) ? m[lm_offset + global_row_id] : -INFINITY;
+            float row_l_prev = (global_row_id < N) ? l[lm_offset + global_row_id] : 0.0f;
 
-            float row_m_prev = m[lm_offset + (tile_row_id * Br) + thread_id];
-            float row_l_prev = l[lm_offset + (tile_row_id * Br) + thread_id];
-
-            // S = QK^T, row_m = rowmax(S)
+            // Compute S
             float row_m = -INFINITY;
-            for (int column_id = 0; column_id < Bc; column_id++)
-            {
+            for (int column_id = 0; column_id < Bc; column_id++) {
+                int global_col_id_inner = tile_column_id * Bc + column_id;
                 float sum = 0.0f;
-                for (int dim_id = 0; dim_id < d; dim_id++)
-                {
-                    sum += Qi[(thread_id * d) + dim_id] * Kj[(column_id * d) + dim_id];
+                for (int dim_id = 0; dim_id < d; dim_id++) {
+                    sum += Qi[thread_id * d + dim_id] * Kj[column_id * d + dim_id];
                 }
                 sum *= softmax_scale;
-                S[(Bc * thread_id) + column_id] = sum;
-
-                if (sum > row_m)
-                    row_m = sum;
-            }
-
-            // P = exp(S - row_m), row_l = rowsum(P)
-            float row_l = 0.0f;
-            for (int seq_id = 0; seq_id < Bc; seq_id++)
-            {
-                S[(thread_id * Bc) + seq_id] = __expf(S[(thread_id * Bc) + seq_id] - row_m);
-                row_l += S[(thread_id * Bc) + seq_id];
-            }
-
-            // Compute new m and l
-            float row_m_new = fmaxf(row_m_prev, row_m);
-            float row_l_new = (__expf(row_m_prev - row_m_new) * row_l_prev) + (__expf(row_m - row_m_new) * row_l);
-
-
-            // Write O, l, m to HBM
-            for (int dim_id = 0; dim_id < d; dim_id++)
-            {
-                float res = 0.0f;
-                for (int seq_id = 0; seq_id < Bc; seq_id++)
-                {
-                    res += S[(thread_id * Bc) + seq_id] * Vj[(seq_id * d) + dim_id];
+                if (global_col_id_inner < N) {
+                    S[thread_id * Bc + column_id] = sum;
+                    if (sum > row_m) row_m = sum;
+                } else {
+                    S[thread_id * Bc + column_id] = -INFINITY;
                 }
-                O[qkv_offset + (tile_row_id * tile_size) + thread_id * d + dim_id] = (1 / row_l_new) \
-                    * ((row_l_prev * __expf(row_m_prev - row_m_new) * O[qkv_offset + (tile_size * tile_row_id) + (thread_id * d) + dim_id]) \
-                    + (__expf(row_m - row_m_new) * res));
             }
 
-            m[lm_offset + (Br * tile_row_id) + thread_id] = row_m_new;
-            l[lm_offset + (Br * tile_row_id) + thread_id] = row_l_new;
+            // Compute P, row_l
+            float row_l = 0.0f;
+            for (int column_id = 0; column_id < Bc; column_id++) {
+                float val = S[thread_id * Bc + column_id];
+                val = __expf(val - row_m);
+                S[thread_id * Bc + column_id] = val;
+                row_l += val;
+            }
+
+            float row_m_new = fmaxf(row_m_prev, row_m);
+            float row_l_new = __expf(row_m_prev - row_m_new) * row_l_prev
+                            + __expf(row_m - row_m_new) * row_l;
+
+            // Accumulate output
+            if (global_row_id < N) {
+                for (int dim_id = 0; dim_id < d; dim_id++) {
+                    float res = 0.0f;
+                    for (int column_id = 0; column_id < Bc; column_id++) {
+                        res += S[thread_id * Bc + column_id] * Vj[column_id * d + dim_id];
+                    }
+                    O[qkv_offset + global_row_id * d + dim_id] = (1.0f / row_l_new) *
+                        (row_l_prev * __expf(row_m_prev - row_m_new) * O[qkv_offset + global_row_id * d + dim_id] +
+                         __expf(row_m - row_m_new) * res);
+                }
+                m[lm_offset + global_row_id] = row_m_new;
+                l[lm_offset + global_row_id] = row_l_new;
+            }
         }
         __syncthreads();
     }
 }
 
-
 torch::Tensor flash_attention(torch::Tensor Q, torch::Tensor K, torch::Tensor V)
 {
-
     const int Bc = 32;
     const int Br = 32;
 
@@ -108,17 +118,15 @@ torch::Tensor flash_attention(torch::Tensor Q, torch::Tensor K, torch::Tensor V)
     const int N = Q.size(2);
     const int d = Q.size(3);
 
+    const int Tc = ceilf((float) N / Bc);
+    const int Tr = ceilf((float) N / Br);
+    const float softmax_scale = 1.0f / sqrtf((float)d);
 
-    const int Tc = ceil((float) N / Bc);
-    const int Tr = ceil((float) N / Br);
-    const float softmax_scale = 1.0f / sqrt(d);
-
-    // Initialise O, l, m to HBM
     torch::Tensor O = torch::zeros({B, nh, N, d}, Q.options());
     torch::Tensor l = torch::zeros({B, nh, N}, Q.options());
     torch::Tensor m = torch::full({B, nh, N}, -INFINITY, Q.options());
 
-    const int sram_size = (3 * Bc * d  + Bc * Br) * sizeof(float);
+    const int sram_size = (3 * Bc * d + Bc * Br) * sizeof(float);
     int max_sram_size;
     cudaDeviceGetAttribute(&max_sram_size, cudaDevAttrMaxSharedMemoryPerBlock, 0);
     printf("Max shared memory: %d, requested shared memory: %d \n", max_sram_size, sram_size);
