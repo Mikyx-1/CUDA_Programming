1ef2cc16981b7dbd9b113518b98b92b871494e65
le_hoang_viet <le_hoang_viet@monorevo.jp>
2025-07-06 06:41:59 +0700

    update the test.py for accurate benchmarking

--- add/test.py
+++ add/test.py
@@ -1,22 +1,44 @@
-import math
-
 import torch
+import torch.nn.functional as F
 from torch.utils.cpp_extension import load
 
 ext = load(
     name="add",
-    sources=["./add/main.cpp", "./add/add_advanced.cu"],
+    sources=["./add/main.cpp", "./add/relu.cu"],
     extra_cuda_cflags=["-O2"],
     extra_cflags=["-O2"],
 )
 
-DEVICE = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-a = torch.randn((1, 3, 6, 4)).to(DEVICE)
-b = torch.randn((1, 3, 6, 4)).to(DEVICE)
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+dummy = torch.randn((32, 512, 256, 128), device=DEVICE)
+NUM_ITERS = 1
 
-res = ext.add(a, b)
-res1 = a + b
+# Create CUDA events
+start = torch.cuda.Event(enable_timing=True)
+end = torch.cuda.Event(enable_timing=True)
 
-print(f"res: {res}")
-print(f"res1: {res1}")
-print(f"all_close: {torch.allclose(res, res1, atol=1e-7)}")
+# ==============================
+# Benchmark PyTorch ReLU
+# ==============================
+start.record()
+for _ in range(NUM_ITERS):
+    res = F.relu(dummy, False)
+end.record()
+
+# Wait for everything to finish
+torch.cuda.synchronize()
+# Compute elapsed time (ms)
+elapsed_ms = start.elapsed_time(end)
+print(f"PyTorch: {elapsed_ms / 1000.0:.6f} seconds")
+
+# ==============================
+# Benchmark your CUDA ReLU
+# ==============================
+start.record()
+for _ in range(NUM_ITERS):
+    res1 = ext.relu(dummy, False)
+end.record()
+
+torch.cuda.synchronize()
+elapsed_ms = start.elapsed_time(end)
+print(f"CUDA: {elapsed_ms / 1000.0:.6f} seconds")
